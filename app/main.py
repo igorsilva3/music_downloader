ae48188f7f1fdc2d99c36ab281fdf38bcf94940a
Igor Silva <igorbr2018@gmail.com>
2022-03-14 19:09:55 -0300

    Created menu of interaction for user

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
+from .core.downloader import Downloader
+from .core.media import Media
+
+
 class App: 
-  def run(self):
-    print('Hello World')
+  def download_music(self, url: str) -> Path:
+    media = Media(url)
+    downloader = Downloader(media)
+    return downloader.start_download()
--- main.py
+++ main.py
@@ -2,10 +2,29 @@ import sys
 
 from app.main import App
 
+app = App()
+
+def menu():
+    print("""
+		1. Download of music by url video
+		2. Quit
+	""")
+
+    option = str(input("Enter your option: "))
+
+    if option == "1":
+        url = str(input("Enter the url video: "))
+        app.download_music(url)
+        print("\nDownload complete!")
+    elif option == "2":
+        sys.exit()
+
+
 def main():
-  app = App()
-  app.run()
+
+    menu()
+
 
 if __name__ == '__main__':
-  main()
-  sys.exit()
+    main()
+    sys.exit()
